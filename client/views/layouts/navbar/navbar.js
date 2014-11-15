20c751bc2a0d42b8e719eddfe733201a6ee8cb72
Yann <yann.buydens@gmail.com>
2014-11-15 13:48:51 +0100

    got user login working

---
+++ client/views/layouts/navbar/navbar.js
@@ -0,0 +1,35 @@
+
+/*****************************************************************************/
+/* Navbar: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.Navbar.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.Navbar.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Navbar: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Navbar.created = function () {
+};
+
+Template.Navbar.rendered = function () {
+};
+
+Template.Navbar.destroyed = function () {
+};
+
+
